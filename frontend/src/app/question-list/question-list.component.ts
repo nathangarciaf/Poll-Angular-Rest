872cdf4bcc8ad57ac65dc03712695a68b8bba33e
nath <nathangarfreitas@gmail.com>
2024-05-16 17:40:10 -0300

    'new'

--- frontend/src/app/question-list/question-list.component.ts
+++ frontend/src/app/question-list/question-list.component.ts
@@ -2,18 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { PollService } from '../services/poll.service';
+import { Router, RouterLink } from '@angular/router';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-question-list',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [NgFor, NgIf, RouterLink],
   templateUrl: './question-list.component.html',
   styleUrls: ['./question-list.component.css']
 })
 export class QuestionListComponent {
   items: any[] = [];
 
-  constructor(private pollService: PollService) { }
+  constructor(private pollService: PollService, private router: Router) { }
 
   ngOnInit(): void {
     this.pollService.getQuestions().subscribe(data => {
